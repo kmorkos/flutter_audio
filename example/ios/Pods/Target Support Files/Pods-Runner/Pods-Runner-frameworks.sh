cdb7c464713530e739388df7143ebf0ad274d201
rxlabz <dev@rxlabz.com>
2017-05-25 12:06:03 +0200

    update readme & pubspec

--- example/ios/Pods/Target Support Files/Pods-Runner/Pods-Runner-frameworks.sh
+++ example/ios/Pods/Target Support Files/Pods-Runner/Pods-Runner-frameworks.sh
@@ -89,11 +89,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/../../../../../../../tools/flutter/bin/cache/artifacts/engine/ios/Flutter.framework"
+  install_framework "${PODS_ROOT}/../../../../../../tools/flutter/bin/cache/artifacts/engine/ios/Flutter.framework"
   install_framework "$BUILT_PRODUCTS_DIR/audioplayer/audioplayer.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/../../../../../../../tools/flutter/bin/cache/artifacts/engine/ios/Flutter.framework"
+  install_framework "${PODS_ROOT}/../../../../../../tools/flutter/bin/cache/artifacts/engine/ios/Flutter.framework"
   install_framework "$BUILT_PRODUCTS_DIR/audioplayer/audioplayer.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
